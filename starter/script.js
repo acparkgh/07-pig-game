0a23c6af96d7600af3a880f5aa0f0fc81ae01a07
acparkgh <acpark123@gmail.com>
2021-04-24 19:12:12 -0400

    Roll dice and switch player

--- starter/script.js
+++ starter/script.js
@@ -1,15 +1,46 @@
 'use strict';
 
-const diceEl = document.getElementsByClassName('dice');
+const diceEl = document.querySelector('.dice');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const btnNewGame = document.querySelector('.btn--new');
 
-diceEl[0].classList.add('hidden');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const currentScorePlayer0El = document.querySelector('#current--0');
+const currentScorePlayer1El = document.getElementById('current--1');
+
+let currentScore = 0;
+let activePlayer = 0;
+let scores = [0, 0];
+
+
+diceEl.classList.add('hide--element');
 score0El.textContent = 0;
 score1El.textContent = 0;
 
-// btnNewGame.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
+  
+  const randomDiceNumber = Math.trunc((Math.random() * 6)) + 1;
+  diceEl.src = `dice-${randomDiceNumber}.png`;
+  diceEl.classList.remove('hide--element');
+  
+  if (randomDiceNumber !== 1) {
+    currentScore += randomDiceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    
+    activePlayer = (activePlayer === 0 ? 1 : 0);
+    
+    // document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+    document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
+    currentScore = 0;
+  }
+  
+});
 
-// });
+
 
