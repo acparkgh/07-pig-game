a5422abb64506cad5f795d84bebed008ff5f32af
acparkgh <acpark123@gmail.com>
2021-04-24 02:29:11 -0400

    set initial scores to 0 and hide dice

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const diceEl = document.getElementsByClassName('dice');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const btnNewGame = document.querySelector('.btn--new');
+
+diceEl[0].classList.add('hidden');
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+// btnNewGame.addEventListener('click', function () {
+
+// });
+
